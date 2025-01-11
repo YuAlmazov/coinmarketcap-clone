92678cb8280210ebec3684c6b97be0460e16c4fd
yualmazov <yualmazov@gmail.com>
2025-01-12 01:57:03 +0300

    Сохранение темы в localforage

--- src/components/ThemeMenu.tsx
+++ src/components/ThemeMenu.tsx
@@ -1,50 +1,72 @@
 'use client';
 
-import { ActionIcon, Button, Menu, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, Menu, useMantineColorScheme } from '@mantine/core';
 import { IconMoon, IconSun } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
+import localforage from 'localforage';
+
+const THEME_KEY = 'theme_preferred_mode';
 
 const ThemeMenu = () => {
-	const [mounted, setMounted] = useState<boolean>(false);
-	const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
 
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+  // При первом рендере пытаемся прочитать тему из IndexedDB
+  useEffect(() => {
+    let isMounted = true;
+    localforage.getItem<string>(THEME_KEY).then((storedTheme) => {
+      if (isMounted && storedTheme) {
+        setColorScheme(storedTheme);
+      }
+      setMounted(true);
+    });
 
-	if (!mounted) {
-		return <div className="w-[34px] h-[34px]"></div>;
-	}
+    return () => {
+      isMounted = false;
+    };
+  }, [setColorScheme]);
 
-	return (
-		<Menu shadow="md" width={200}>
-			<Menu.Target>
-				<ActionIcon variant="outline" size="lg">
-					{colorScheme === 'dark' ? (
-						<IconMoon size={16} />
-					) : (
-						<IconSun size={16} />
-					)}
-				</ActionIcon>
-			</Menu.Target>
+  // При смене темы записываем в IndexedDB
+  useEffect(() => {
+    if (mounted) {
+      localforage.setItem(THEME_KEY, colorScheme);
+    }
+  }, [colorScheme, mounted]);
 
-			<Menu.Dropdown>
-				<Menu.Label>Theme</Menu.Label>
-				<Menu.Item
-					onClick={() => setColorScheme('light')}
-					leftSection={<IconSun size={16} />}
-				>
-					Light
-				</Menu.Item>
-				<Menu.Item
-					onClick={() => setColorScheme('dark')}
-					leftSection={<IconMoon size={16} />}
-				>
-					Dark
-				</Menu.Item>
-			</Menu.Dropdown>
-		</Menu>
-	);
+  // Пока не прочитали из IndexedDB, возвращаем пустой блок, чтобы не мигала тема
+  if (!mounted) {
+    return <div className="w-[34px] h-[34px]" />;
+  }
+
+  return (
+    <Menu shadow="md" width={200}>
+      <Menu.Target>
+        <ActionIcon variant="outline" size="lg">
+          {colorScheme === 'dark' ? (
+            <IconMoon size={16} />
+          ) : (
+            <IconSun size={16} />
+          )}
+        </ActionIcon>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>Theme</Menu.Label>
+        <Menu.Item
+          onClick={() => setColorScheme('light')}
+          leftSection={<IconSun size={16} />}
+        >
+          Light
+        </Menu.Item>
+        <Menu.Item
+          onClick={() => setColorScheme('dark')}
+          leftSection={<IconMoon size={16} />}
+        >
+          Dark
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
 };
 
 export default ThemeMenu;
