33ef4fc97465acdd8eeefdbda9b0426136eabe00
yualmazov <yualmazov@gmail.com>
2025-01-12 07:18:54 +0300

    Доработки по видео

--- src/components/CryptoCarousel.tsx
+++ src/components/CryptoCarousel.tsx
@@ -132,7 +132,7 @@ export default function CryptoCarousel({
           "
           // Ключевые моменты для полноэкранного режима на мобилке
           allowFullScreen
-   
+          {...{ 'webkitallowfullscreen': '', 'mozallowfullscreen': '' }}
         />
 
         {/* ================ overlay для свайпа ================ */}
